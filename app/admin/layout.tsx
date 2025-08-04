84d1ac6e014e37d56c5e03c02d75ba352ab957e9
Tarun Singh <tarun@Taruns-MacBook-Air.local>
2025-08-04 17:23:59 +0530

    pre pod 1

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from '@/lib/auth'
 import { User } from '@prisma/client'
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AdminLayout({
   children,
 }: {
